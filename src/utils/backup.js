1b429464644f061897d978d745a7be9ec0a0af70
Huu Hoang <huuhoag1412@gmail.com>
2025-03-06 11:54:59 +0700

    feat: Add data export and import functionality to backup utility

---
+++ src/utils/backup.js
@@ -0,0 +1,34 @@
+import dbStorage from '@/db/storage';
+import browser from 'webextension-polyfill';
+
+export async function exportData() {
+  try {
+    const storage = await browser.storage.local.get(null);
+    const storageDb = {};
+    dbStorage.tables.forEach(function (table) {
+      storageDb[table.name] = table.toArray();
+    });
+    return { storage, storageDb };
+  } catch (error) {
+    console.error('Error exporting data:', error);
+    return null;
+  }
+}
+
+export async function importData({ storage, storageDb }) {
+  try {
+    await browser.storage.local.clear();
+    await browser.storage.local.set(storage);
+    await dbStorage.transaction('rw', dbStorage.tables, async function () {
+      Object.keys(storageDb).forEach(async function (tableName) {
+        const table = dbStorage.table(tableName);
+        await table.clear();
+        await table.bulkAdd(storageDb[tableName]);
+      });
+    });
+    return true;
+  } catch (error) {
+    console.error('Error importing data:', error);
+    return false;
+  }
+}
